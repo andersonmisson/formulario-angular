5df6959d7bb95f2a3085c0d7becec9987ac39aad
Anderson Misson <missondeveloper@gmail.com>
2020-12-24 00:30:53 -0300

    Definindo o valor padrão em campos com ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'formulario-angular';
 
-  profissoes = ['Programador', 'Empresário', 'Outra'];
+  profissoes = ['Programador', 'Empresário', 'Outra', 'Teste'];
+  profissao = 'Teste'; // Esse é o NgModel, valor padrão que vai aparecer no OPTIONS
 
   salvar(form: NgForm) {
-    console.log(form);
-    console.log(form.value.nome);
+    console.log(this.profissao);
+
+    console.log(form.value.profissao);
   }
 }
